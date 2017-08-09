eee27dab330f3970f193f9554260260bface2653
Dhvani Patel <dhvanipatel_2000@yahoo.ca>
2017-08-09 14:29:18 -0600

    Update col_finder.py

--- Scripts/col_finder.py
+++ Scripts/col_finder.py
@@ -6,6 +6,14 @@ import sys
 import keyword
 import difflib
 
+# Method for finding index of certain characters in a string, n being the n'th occurence of the character/string
+def find_nth(haystack, needle, n):
+    start = haystack.find(needle)
+    while start >= 0 and n > 1:
+        start = haystack.find(needle, start+len(needle))
+        n -= 1
+    return start
+
 def getCol():
 	rootdir = '/home/dhvani/java-mistakes-data'
 	
@@ -27,12 +35,17 @@ def getCol():
 	# print strPaths
 	assert len(strPathsBef) == len(strPathsAft)
 
-	skip = 3530
+	all_cols = []
+	
+	csvfile = open('java_fixes_col.csv', 'wb')
+	datWriter = csv.writer(csvfile, delimiter=',',
+                        	   quoting=csv.QUOTE_MINIMAL)
+	datWriter.writerow(["sfid", "meid", "line_start", "col_start", "line_end", "col_end"])
 	for loopInd in range(len(strPathsBef)):	
+		beforeLen = len(all_cols)
 		#print loopInd
-		if loopInd+skip == 2785:
+		if loopInd == 2784:
 			continue
-		loopInd += skip	
 		print loopInd
 		beforeStrList = []
 		afterStrList = []
@@ -47,43 +60,58 @@ def getCol():
 			for token in beforeToks:
 				if token[0] in ["DOUBLELITERAL", "FLOATLITERAL"]:
 					beforeStrList.append("INTLITERAL")
+				elif token[0] == "CHARLITERAL":
+					beforeStrList.append("STRINGLITERAL")
 				else:
 					beforeStrList.append(token[0])
 				#print token[1]
 		#print "---------------------"
 		afterToks = []
+	
 		with open(afterF, 'r') as myfile:
     			dataAfter=myfile.read()
 			afterToks = java.lex(dataAfter)
 			for token in afterToks:
 				if token[0] in ["DOUBLELITERAL", "FLOATLITERAL"]:
 					afterStrList.append("INTLITERAL")
+				elif token[0] == "CHARLITERAL":
+					afterStrList.append("STRINGLITERAL")
 				else:
 					afterStrList.append(token[0])
 				#print token[1]
 		#print len(beforeStrList)
 		#print len(afterStrList)
-		
-		print beforeF
-		print afterF
+		sfid = int(beforeF[find_nth(beforeF, "/", 4)+1:find_nth(beforeF, "/", 5)])
+		meid = int(beforeF[find_nth(beforeF, "/", 5)+1:find_nth(beforeF, "/", 6)])
+		#print beforeF
+		#print afterF
 		s = difflib.SequenceMatcher(None,a='', b='',autojunk=False)
 		s.set_seqs(beforeStrList, afterStrList)
 		#assert len(s.get_opcodes()) > 3
-		if len(s.get_opcodes()) > 3:
-			for opcode in s.get_opcodes():
-				print opcode
-				
-				#print beforeStrList[opcode[1]]
-				print beforeToks[opcode[1]]
-				print beforeToks[opcode[3]]
-				print "-----------"
-			
-			print type(radha)
-		
+	
+		for opcode in s.get_opcodes():
+			#print opcode
+			if opcode[0] != "equal":
+				if opcode[0] != "delete":
+					#print type(beforeToks[opcode[3]][2])
+					add = [sfid, meid, beforeToks[opcode[3]][2][0],beforeToks[opcode[3]][2][1], beforeToks[opcode[3]][3][0], beforeToks[opcode[3]][3][1]]
+					#print add
+					datWriter.writerow(add)
+					break
+				else:
+					#print type(beforeToks[opcode[1]][2])
+					add = [sfid, meid, beforeToks[opcode[1]][2][0],beforeToks[opcode[1]][2][1], beforeToks[opcode[1]][3][0], beforeToks[opcode[1]][3][1]]
+					datWriter.writerow(add)
+					break
+
+		#print all_cols
+		#print type(radha)
 	
 
 		#sys.exit()
+	csvfile.close()
 	print "FINISHED"
+	#print all_cols
 
 	#getToks = java.lex(toCompTokD)
 
